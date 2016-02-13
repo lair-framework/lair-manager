d08e7af9ee0b297620d842418961f0bd494a7628
Kyle <iamkgro@gmail.com>
2016-02-12 22:02:23 -0800

    cleanup file

---
+++ cleanup.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+rm -rf deps
+rm mongodb-linux-x86_64-ubuntu1404-3.0.6.tgz
+rm node-v4.2.6-linux-x64.tar.xz
+rm api-server_linux_amd64
+rm caddy_linux_amd64.tar.gz
+rm lair-v2.0.4-linux-amd64.tar.gz
